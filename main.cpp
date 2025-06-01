7d2cc4d0dfe2195405959886caa6800aacbc5b0f
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2025-06-01 14:46:14 -0400

    correctly see folders

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ using std::string;
 using std::vector;
 #include <fstream>
 using std::ifstream; // read data from file
-using std::iofstream; // write data to file
+using std::ofstream; // write data to file
+#include <filesystem>
+using namespace std::filesystem; // check directories and files
 
 /* read arguments of folder
 	sort through folder and pull out each photo
@@ -16,8 +18,15 @@ using std::iofstream; // write data to file
 */
 
 // function to check if file is available or if it is inside another folder
-bool checkFile (string folder)
+void checkFile (string folder)
 {
+	path directoryPath = folder;
+
+	if (exists(directoryPath) && is_directory(directoryPath)) {
+		for (const auto& entry : directory_iterator(directoryPath)) {
+			cout << "file is " << entry.path() << '\n';
+		}
+	} else cout << "Not a directory";
 	// if folder call checkfile
 	// else return the path to the folder
 }
@@ -26,15 +35,22 @@ bool checkFile (string folder)
 vector<string> sort(string folder)
 {
 	// call check file to get to the actual image
+
 	// once at the image add the path to the vector
 	// create a new folder with all of the files
 	// if flagged to delete original, delete original directory
+	return {};
 }
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]){
+	// allow flags for user to create new folder, delete old folder, etc
+
 	string s;
 	cout << "Welcome to the photo organizer\n";
 	cout << "Please enter the path for the folder that you want to organize ";
-	cin >> n; // this is going to be a string of the path to the folder
+	cin >> s; // this is going to be a string of the path to the folder
+
+	cout << "\nSo " << s << " is what you want to update?";
+
+	checkFile(s);
 }
